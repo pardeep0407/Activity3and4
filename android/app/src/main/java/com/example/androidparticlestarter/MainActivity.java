c6d359de9ce7f40904c3d710a0288f851e0c9f24
Pardeep <>
2019-11-04 23:35:41 -0500

    Identifying number of sides in a shape

--- android/app/src/main/java/com/example/androidparticlestarter/MainActivity.java
+++ android/app/src/main/java/com/example/androidparticlestarter/MainActivity.java
@@ -1,137 +1,207 @@
 package com.example.androidparticlestarter;
 
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.speech.RecognizerIntent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
+import java.util.Random;
+
 import io.particle.android.sdk.cloud.ParticleCloud;
 import io.particle.android.sdk.cloud.ParticleCloudSDK;
 import io.particle.android.sdk.cloud.ParticleDevice;
+import io.particle.android.sdk.cloud.ParticleEvent;
+import io.particle.android.sdk.cloud.ParticleEventHandler;
 import io.particle.android.sdk.cloud.exceptions.ParticleCloudException;
 import io.particle.android.sdk.utils.Async;
 
 public class MainActivity extends AppCompatActivity {
-    private final String TAG="PARDEEP";
-    private final int REQ_CODE = 100;
-    ImageButton micro_Phone;
-    private final String PARTICLE_USERNAME = "akshdeepkaur235@gmail.com";
-    private final String PARTICLE_PASSWORD = "9463931734";
-    private final String DEVICE_ID = "1f0036000e47363333343437";
-    private ParticleDevice mDevice;
-    String command1 = "Particle turn on all lights";
-    String command2 ="Particle create a rainbow";
-    String voice_Result = "";
+    private final String TAG="Pardeep";
 
+    int[] images = {R.drawable.triangle,R.drawable.rectangle};
+    ImageView question;
+    Random random_genrator = new Random();;
+    String particleId;
+    String answer = "";
+    private final String PARTICLE_USERNAME = "nm290290@gmail.com";
+    private final String PARTICLE_PASSWORD = "nonsense2";
+    private final String DEVICE_ID = "38001c000f47363333343437";
+    TextView ans_text;
+    private long subscriptionId;
+    private ParticleDevice mDevice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        micro_Phone = findViewById(R.id.micro_phone);
+        question = findViewById(R.id.question2);
+        ans_text = findViewById(R.id.ans);
         ParticleCloudSDK.init(this.getApplicationContext());
+        setImage();
         getDeviceFromCloud();
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to give command to particle");
-        try {
-            startActivityForResult(intent, REQ_CODE);
-        } catch (ActivityNotFoundException a) {
-        }
+        getEvents();
     }
 
-public void speak(View v)
+    public void getEvents()
     {
-        getDeviceFromCloud();
+       getDeviceFromCloud();
+            Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+                @Override
+                public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                    try {
+                        subscriptionId = ParticleCloudSDK.getCloud().subscribeToAllEvents("answer", new ParticleEventHandler() {
+                            @Override
+                            public void onEventError(Exception e) {
+                            }
+                            @Override
+                            public void onEvent(String eventName, ParticleEvent particleEvent) {
+                                particleId = particleEvent.deviceId;
+                                    answer = particleEvent.dataPayload;
+                            }
+                        });
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to give command to particle");
-        try {
-            startActivityForResult(intent, REQ_CODE);
-        } catch (ActivityNotFoundException a) {
-           }
+                    return -1;
+                }
+
+                @Override
+                public void onSuccess(Object o) {
+
+                }
+
+                @Override
+                public void onFailure(ParticleCloudException exception) {
+                }
+            });
+    }
+    public void gameStart(View v)
+    {
+        setImage();
+        getDeviceFromCloud();
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case REQ_CODE: {
-                if (resultCode == RESULT_OK && null != data) {
-                    ArrayList result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    voice_Result = result.get(0).toString();
-                    Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
-                        @Override
-                        public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+    public void setImage() {
 
-                            List<String> param = new ArrayList<String>();
-
-                            if (voice_Result.equalsIgnoreCase(command1))
-                            {
-                                   param.add("command1");
-                            }
-
-                            else if (voice_Result.equalsIgnoreCase(command2))
-                            {
-                                param.add("command2");
-                            }
-
-                            try {
-
-                                mDevice.callFunction("speak",param);
-                            }
-                            catch(Exception e)
-                            {
-                            }
-                            return -1;
-                        }
-                        @Override
-                        public void onSuccess(Object o) {
-                            Log.d(TAG, "Successfully got device from Cloud");
-                        }
-
-                        @Override
-                        public void onFailure(ParticleCloudException exception) {
-                            Log.d(TAG, exception.getBestMessage());
-                        }
-                    });
+        int number = random_genrator.nextInt(2);
+        question.setImageResource(images[number]);;
+        question.setTag(images[number]);
+    }
+    public void checkResult(View v)
+    {
+        if (answer.equals("3") && ((int)question.getTag() == R.drawable.triangle))
+        {
+            Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+                @Override
+                public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                    List<String> functionParameters = new ArrayList<String>();
+                    functionParameters.add("true");
+                    try {
+                        mDevice.callFunction("myAnswer", functionParameters);
+                    } catch (ParticleDevice.FunctionDoesNotExistException e) {
+                        e.printStackTrace();
+                    }
+                    return -1;
                 }
-            }
+                @Override
+                public void onSuccess(Object o) {
+                    ans_text.setText("Correct Answer");
+
+                }
+                @Override
+                public void onFailure(ParticleCloudException exception) {
+                    Log.d(TAG, exception.getBestMessage());
+                }
+            });
+        }
+
+       else if (answer.equals("4") && ((int)question.getTag() == R.drawable.rectangle))
+        {
+            Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+                @Override
+                public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                    List<String> functionParameters = new ArrayList<String>();
+                    functionParameters.add("true");
+                    try {
+                        mDevice.callFunction("myAnswer", functionParameters);
+
+                    } catch (ParticleDevice.FunctionDoesNotExistException e) {
+                        e.printStackTrace();
+                    }
+                    return -1;
+                }
+                @Override
+                public void onSuccess(Object o) {
+                    ans_text.setText("Correct Answer");
+
+                }
+                @Override
+                public void onFailure(ParticleCloudException exception) {
+                    Log.d(TAG, exception.getBestMessage());
+                }
+            });
+        }
+
+       else
+        {
+            Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+                @Override
+                public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                    List<String> functionParameters = new ArrayList<String>();
+                    functionParameters.add("false");
+                    try {
+                        mDevice.callFunction("myAnswer", functionParameters);
+
+                    } catch (ParticleDevice.FunctionDoesNotExistException e) {
+                        e.printStackTrace();
+                    }
+                    return -1;
+                }
+                @Override
+                public void onSuccess(Object o) {
+                    ans_text.setText("Incorrect Answer");
+
+                }
+                @Override
+                public void onFailure(ParticleCloudException exception) {
+                    Log.d(TAG, exception.getBestMessage());
+                }
+            });
         }
     }
 
     public void getDeviceFromCloud() {
         Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
-
             @Override
             public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
                 particleCloud.logIn(PARTICLE_USERNAME, PARTICLE_PASSWORD);
                 mDevice = particleCloud.getDevice(DEVICE_ID);
+                try {
+                }
+                catch(Exception e)
+                {
+                }
+
                 return -1;
             }
             @Override
             public void onSuccess(Object o) {
-                Log.d(TAG, "Successfully got device from Cloud");
             }
 
-            @Override
-            public void onFailure(ParticleCloudException exception) {
-                Log.d(TAG, exception.getBestMessage());
+            @Override public void onFailure(ParticleCloudException exception) {
             }
         });
     }
+
 
 }
